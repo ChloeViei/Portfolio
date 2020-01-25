263a1b929872589fb79a018375b2e9bd6357268c
Chloé Vieille <chloevieille@hotmail.com>
2020-01-25 21:56:23 +0100

    Creation de la section contact

--- js/main.js
+++ js/main.js
@@ -60,6 +60,20 @@
 		});
 	}
 
+	/*--/ Projects /--*/
+
+	$('.gallery ul li a').click(function() {
+		var itemID = $(this).attr('href');
+		$('.gallery ul').addClass('item_open');
+		$(itemID).addClass('item_open');
+		return false;
+	});
+	$('.close').click(function() {
+		$('.port, .gallery ul').removeClass('item_open');
+		return false;
+	});
+
+
 
 
 	/*--/ Star Scrolling nav /--*/
